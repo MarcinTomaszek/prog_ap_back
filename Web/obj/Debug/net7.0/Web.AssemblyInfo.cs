591bb037b5e436bd4acdecfd5d5c07b1ab9f5d95
marcin.tomaszek <marcin.tomaszek@microsoft.wsei.edu.pl>
2025-02-25 15:50:31 +0100

    init

--- Web/obj/Debug/net7.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net7.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48f4deb9561d76d87b2c08ad86dcdb59cd050e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e6233a7fc8bcfdf463b24fd9dcd0e747bebaee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
