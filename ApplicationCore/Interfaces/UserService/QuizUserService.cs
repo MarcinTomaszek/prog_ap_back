0130e5ec8249f3bc0b2edd95b16fa74dccad04f5
BlueJack14 <bluejack1414@gmail.com>
2025-03-10 15:03:14 +0100

    Task completed. Private messages added.

--- ApplicationCore/Interfaces/UserService/QuizUserService.cs
+++ ApplicationCore/Interfaces/UserService/QuizUserService.cs
@@ -36,10 +36,6 @@ public class QuizUserService: IQuizUserService
 
     public List<QuizItemUserAnswer> GetUserAnswersForQuiz(int quizId, int userId)
     {
-        // return answerRepository.FindAll()
-        //     .Where(x => x.QuizId == quizId)
-        //     .Where(x => x. UserId == userId)
-        //     .ToList();
         return answerRepository.FindBySpecification(new QuizItemsForQuizIdFilledByUser(quizId, userId)).ToList();
     }
 }
---
+++ BlazorChat/Program.cs
@@ -0,0 +1,31 @@
+using ApplicationCore.Interfaces.Repository;
+using BackendLab01;
+using BlazorChat;
+using BlazorChat.Components;
+using Infrastructure.Memory;
+using Infrastructure.Memory.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();               
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddTransient<IGenericGenerator<int>,IntGenerator>();
+builder.Services.AddSingleton<IGenericRepository<ChatUser, int>,MemoryGenericRepository<ChatUser, int>>();
+builder.Services.AddSingleton<IChatUserService,ChatUserService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+app.MapHub<BlazorChatSampleHub>(BlazorChatSampleHub.HubUrl); 
+app.Run();
